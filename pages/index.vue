2d93a9aa73efe3a814691a9e212f8e097277c49d
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-07-26 17:50:41 +0200

    draggable cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,64 @@
 <template>
-  <div class="container">
-    <div class="card">
-      <fa class="icon" icon="code" />
-      <h1>Titre de ma carte</h1>
-      <p>Description et contenu</p>
-    </div>
-    <div class="card card-2">
-      <fa class="icon" icon="database" />
-      <h1>Titre de ma seconde carte</h1>
-      <p>Description et contenu 2</p>
-    </div>
+  <div>
+    <grid-layout
+      :layout.sync="layout"
+      :col-num="12"
+      :row-height="30"
+      :is-draggable="draggable"
+      :is-resizable="resizable"
+      :responsive="responsive"
+      :vertical-compact="true"
+      :use-css-transforms="true"
+    >
+      <grid-item
+        v-for="item in layout"
+        :static="item.static"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+        :key="item.i"
+      >
+        <span class="text">{{ item.i }}</span>
+      </grid-item>
+    </grid-layout>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      layout: [
+        { x: 0, y: 0, w: 2, h: 2, i: "0" },
+        { x: 2, y: 0, w: 2, h: 4, i: "1" },
+        { x: 4, y: 0, w: 2, h: 5, i: "2" },
+        { x: 6, y: 0, w: 2, h: 3, i: "3" },
+        { x: 8, y: 0, w: 2, h: 3, i: "4" },
+        { x: 10, y: 0, w: 2, h: 3, i: "5" },
+        { x: 0, y: 5, w: 2, h: 5, i: "6" },
+        { x: 2, y: 5, w: 2, h: 5, i: "7" },
+        { x: 4, y: 5, w: 2, h: 5, i: "8" },
+        { x: 6, y: 4, w: 2, h: 4, i: "9" },
+        { x: 8, y: 4, w: 2, h: 4, i: "10" },
+        { x: 10, y: 4, w: 2, h: 4, i: "11" },
+        { x: 0, y: 10, w: 2, h: 5, i: "12" },
+        { x: 2, y: 10, w: 2, h: 5, i: "13" },
+        { x: 4, y: 8, w: 2, h: 4, i: "14" },
+        { x: 6, y: 8, w: 2, h: 4, i: "15" },
+        { x: 8, y: 10, w: 2, h: 5, i: "16" },
+        { x: 10, y: 4, w: 2, h: 2, i: "17" },
+        { x: 0, y: 9, w: 2, h: 3, i: "18" },
+        { x: 2, y: 6, w: 2, h: 2, i: "19" }
+      ],
+      draggable: true,
+      resizable: false,
+      responsive: true,
+      index: 0
+    };
+  }
+};
 </script>
 
 <style scoped>
@@ -53,5 +97,89 @@ export default {};
 
 .card p {
   margin: 0;
+}
+
+.special-card {
+  padding: 20px;
+  border-radius: 5px;
+  background: white;
+  font-family: "Quicksand", sans-serif;
+  border: solid gray 2px;
+}
+
+.special-card .icon {
+  margin: 0;
+  margin-bottom: 10px;
+  font-size: 25px;
+  color: rgb(50, 50, 50);
+}
+
+.special-card h1 {
+  margin: 0;
+}
+
+.special-card p {
+  margin: 0;
+}
+
+/* grid layout */
+
+.vue-grid-layout {
+  background: #eee;
+}
+
+.vue-grid-item:not(.vue-grid-placeholder) {
+  background: #ccc;
+  border: 1px solid black;
+}
+
+.vue-grid-item .resizing {
+  opacity: 0.9;
+}
+
+.vue-grid-item .static {
+  background: #cce;
+}
+
+.vue-grid-item .text {
+  font-size: 24px;
+  text-align: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  height: 100%;
+  width: 100%;
+}
+
+.vue-grid-item .no-drag {
+  height: 100%;
+  width: 100%;
+}
+
+.vue-grid-item .minMax {
+  font-size: 12px;
+}
+
+.vue-grid-item .add {
+  cursor: pointer;
+}
+
+.vue-draggable-handle {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  top: 0;
+  left: 0;
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>")
+    no-repeat;
+  background-position: bottom right;
+  padding: 0 8px 8px 0;
+  background-repeat: no-repeat;
+  background-origin: content-box;
+  box-sizing: border-box;
+  cursor: pointer;
 }
 </style>
---
+++ plugins/vue-grid-layout.js
@@ -0,0 +1,5 @@
+import Vue from "vue";
+import VueGridLayout from "vue-grid-layout";
+
+Vue.component("GridLayout", VueGridLayout.GridLayout);
+Vue.component("GridItem", VueGridLayout.GridItem);
