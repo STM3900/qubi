d53dbf2f07e8154bf148df9b687c07c37a57e6a7
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-07-26 13:42:50 +0200

    simple card

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,41 +1,82 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'qubi',
+    title: "qubi",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    [
+      "nuxt-fontawesome",
+      {
+        component: "fa", //customize component name
+        imports: [
+          {
+            set: "@fortawesome/free-solid-svg-icons",
+            icons: [
+              "faEnvelope",
+              "faFileAlt",
+              "faAddressBook",
+              "faClipboardList",
+              "faCode",
+              "faUserCircle",
+              "faDatabase",
+              "faStar",
+              "faHome",
+              "faIdCard"
+            ]
+          },
+          {
+            set: "@fortawesome/free-brands-svg-icons",
+            icons: [
+              "faLinkedinIn",
+              "faGithub",
+              "faVuejs",
+              "faAngular",
+              "faPhp",
+              "faJsSquare",
+              "faCuttlefish",
+              "faCss3",
+              "faPython"
+            ]
+          },
+          { set: "@fortawesome/free-regular-svg-icons", icons: [] }
+        ]
+      }
+    ],
+    "@nuxtjs/dotenv",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Quicksand: true
+        }
+      }
+    ]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>
-    qubi
+  <div class="container">
+    <div class="card">
+      <fa class="icon" icon="code" />
+      <h1>Titre de ma carte</h1>
+      <p>Description et contenu</p>
+    </div>
+    <div class="card card-2">
+      <fa class="icon" icon="database" />
+      <h1>Titre de ma seconde carte</h1>
+      <p>Description et contenu 2</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-content: center;
+  gap: 15px 25px;
+}
+
+.card {
+  padding: 20px;
+  border-radius: 5px;
+  background: white;
+  font-family: "Quicksand", sans-serif;
+  width: 500px;
+}
+
+.card-2 {
+  width: 800px;
+}
+
+.card .icon {
+  margin: 0;
+  margin-bottom: 10px;
+  font-size: 25px;
+  color: rgb(50, 50, 50);
+}
+
+.card h1 {
+  margin: 0;
+}
+
+.card p {
+  margin: 0;
+}
+</style>
